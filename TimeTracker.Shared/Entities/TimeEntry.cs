8b15d5572f4677a0b8eb5ad354f2c79e5c20fcfa
darko.gelevski <darkogele@hotmail.com>
2024-08-29 16:39:24 +0200

    Add project files.

---
+++ TimeTracker.Shared/Entities/TimeEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace TimeTracker.Shared.Entities;
+
+public class TimeEntry
+{
+    public int Id { get; set; }
+
+    public required string Project { get; set; }
+
+    public DateTime Start { get; set; } = DateTime.Now;
+
+    public DateTime? End { get; set; }
+
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    public DateTime? UpdatedDate { get; set; }
+}
---
+++ TimeTrackerApi/Controllers/TimeEntryController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTrackerApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TimeEntryController : ControllerBase
+{
+    //private readonly ITimeEntryRepository _timeEntryRepository;
+
+    //public TimeEntryController(ITimeEntryRepository timeEntryRepository)
+    //{
+    //    _timeEntryRepository = timeEntryRepository;
+    //}
+
+    //[HttpGet]
+    //public async Task<ActionResult<IEnumerable<TimeEntry>>> Get()
+    //{
+    //    return Ok(await _timeEntryRepository.GetTimeEntries());
+    //}
+
+    //[HttpGet("{id}")]
+    //public async Task<ActionResult<TimeEntry>> Get(int id)
+    //{
+    //    var timeEntry = await _timeEntryRepository.GetTimeEntry(id);
+    //    if (timeEntry == null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    return Ok(timeEntry);
+    //}
+
+    //[HttpPost]
+    //public async Task<ActionResult<TimeEntry>> Post(TimeEntry timeEntry)
+    //{
+    //    await _timeEntryRepository.AddTimeEntry(timeEntry);
+    //    return CreatedAtAction(nameof(Get), new { id = timeEntry.Id }, timeEntry);
+    //}
+
+    //[HttpPut("{id}")]
+    //public async Task<IActionResult> Put(int id, TimeEntry timeEntry)
+    //{
+    //    if (id != timeEntry.Id)
+    //    {
+    //        return BadRequest();
+    //    }
+
+    //    await _timeEntryRepository.UpdateTimeEntry(timeEntry);
+    //    return NoContent();
+    //}
+
+    //[HttpDelete("{id}")]
+    //public async Task<IActionResult> Delete(int id)
+    //{
+    //    var timeEntry = await _timeEntryRepository.GetTimeEntry(id);
+    //    if (timeEntry == null)
+    //    {
+    //        return NotFound();
+    //    }
+
+    //    await _timeEntryRepository.DeleteTimeEntry(id);
+    //    return NoContent();
+    //}
+
+    private static List<TimeEntry> _timeEntries = new List<TimeEntry>
+    {
+        new() { Id = 1, Project = "Time Tracker App", Start = DateTime.Now.AddHours(-2), End = DateTime.Now.AddHours(-1) },
+        new() { Id = 2, Project = "Project 2", Start = DateTime.Now.AddHours(-1), End = DateTime.Now }
+    };
+
+    [HttpGet]
+    public ActionResult<List<TimeEntry>> GetALlTimeEntries()
+    {
+        return Ok(_timeEntries);
+    }
+}
